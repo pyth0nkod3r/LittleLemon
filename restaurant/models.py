2a2fca998b812f5953c575eaa75898969ab27f82
Miracle Anyanwu <mac.mkboy1@gmail.com>
2024-05-05 04:55:12 +0100

    added models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=6)
+    booking_date = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    inventory = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.title
